f3a0098f9396b1eb94835d50762b6b469a57aafc
ArturGrigoryan <you@example.com>
2017-12-19 22:46:18 +0400

    canvas true sort positions and add canvas bg image

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,30 +1,69 @@
 var stage = new createjs.Stage('campingCVN');
 var shape = new createjs.Shape();
 var gr = new createjs.Graphics();
-shape.graphics.beginStroke("teal").arc(100, 100, 50, 0, Math.PI*2);
+shape.graphics.beginStroke("white").arc(100, 100, 50, 0, Math.PI*2);
 stage.addChild(shape);
 shape.x = stage.canvas.width/2-100;
 shape.y = stage.canvas.height/2-100;
 var shape2 = new createjs.Shape();
-shape2.graphics.beginStroke("teal").arc(100, 100, 250, 0, Math.PI*2);
+shape2.graphics.beginStroke("white").arc(100, 100, 250, 0, Math.PI*2);
 shape2.x=stage.canvas.width/2-100;
 shape2.y=stage.canvas.height/2-100;
 stage.addChild(shape2);
 stage.update();
-shape3 = new createjs.Shape();
-for(var a = 0, round = 250; a <= 10; a++, round-=50)
+var shape3 = new createjs.Shape();
+for(var a = 250; a > 0; a-=100)
 {
-    console.log(round)
-    shape3.graphics.beginStroke("teal").arc(shape2.x+100, shape2.y+round+100, 10, 0, Math.PI*2);
-    console.log(shape3.regX)
+    shape3.graphics.beginStroke("teal").arc(Math.sqrt((250*250)-(a*a))+shape2.x+100, (250-a)+135, 50, 0, Math.PI*2);
+    stage.addChild(shape3);
+    stage.update();
+}
+for(var a = 250; a > 0; a-=100)
+{
+    shape3.graphics.beginStroke("teal").arc(Math.sqrt((250*250)-(a*a))+shape2.x+100, a+100+shape2.y, 50, 0, Math.PI*2);
+    stage.addChild(shape3);
+    stage.update();
+}
+for(var a = 250; a > 0; a-=100)
+{
+    shape3.graphics.beginStroke("teal").arc((Math.sqrt((250*250)-(a*a))*-1)+shape2.x+100, (250-a)+135, 50, 0, Math.PI*2);
+    stage.addChild(shape3);
+    stage.update();
+}
 
-}
-stage.addChild(shape3);
-stage.update();
-for(var a = 250; a > 0; a-=50)
+for(var a = 250; a > 0; a-=100)
 {
-   shape3.graphics.arc(Math.sqrt(250*250-a*a)+100+shape2.x, a+100-shape2.y, 10, 0, Math.PI*2);
-   console.log((Math.sqrt(250*250-a*a)+100+shape2.x)+" : "+(a));
+    shape3.graphics.beginStroke("teal").arc((Math.sqrt((250*250)-(a*a))*-1)+shape2.x+100, a+100+shape2.y, 50, 0, Math.PI*2);
+    stage.addChild(shape3);
+    stage.update();
 }
-stage.addChild(shape3);
-stage.update();
+setTimeout(function(){
+    var bitmap = new createjs.Bitmap("images/slider/5.jpg");
+    stage.addChild(bitmap);
+    stage.update();
+    for(var a = 250; a > 0; a-=100)
+    {
+        shape3.graphics.beginStroke("teal").arc(Math.sqrt((250*250)-(a*a))+shape2.x+100, (250-a)+135, 50, 0, Math.PI*2);
+        stage.addChild(shape3);
+        stage.update();
+    }
+    for(var a = 250; a > 0; a-=100)
+    {
+        shape3.graphics.beginStroke("teal").arc(Math.sqrt((250*250)-(a*a))+shape2.x+100, a+100+shape2.y, 50, 0, Math.PI*2);
+        stage.addChild(shape3);
+        stage.update();
+    }
+    for(var a = 250; a > 0; a-=100)
+    {
+        shape3.graphics.beginStroke("teal").arc((Math.sqrt((250*250)-(a*a))*-1)+shape2.x+100, (250-a)+135, 50, 0, Math.PI*2);
+        stage.addChild(shape3);
+        stage.update();
+    }
+
+    for(var a = 250; a > 0; a-=100)
+    {
+        shape3.graphics.beginStroke("teal").arc((Math.sqrt((250*250)-(a*a))*-1)+shape2.x+100, a+100+shape2.y, 50, 0, Math.PI*2);
+        stage.addChild(shape3);
+        stage.update();
+    }
+},4500);
