c1fed401554528c8f09288f9fd455864f5f176cb
ArturGrigoryan <you@example.com>
2017-12-20 23:50:56 +0400

    add image to canvas

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -10,60 +10,45 @@ shape2.graphics.beginStroke("white").arc(100, 100, 250, 0, Math.PI*2);
 shape2.x=stage.canvas.width/2-100;
 shape2.y=stage.canvas.height/2-100;
 stage.addChild(shape2);
-stage.update();
 var shape3 = new createjs.Shape();
-for(var a = 250; a > 0; a-=100)
+function createImg(x, y)
 {
-    shape3.graphics.beginStroke("teal").arc(Math.sqrt((250*250)-(a*a))+shape2.x+100, (250-a)+135, 50, 0, Math.PI*2);
-    stage.addChild(shape3);
-    stage.update();
-}
-for(var a = 250; a > 0; a-=100)
-{
-    shape3.graphics.beginStroke("teal").arc(Math.sqrt((250*250)-(a*a))+shape2.x+100, a+100+shape2.y, 50, 0, Math.PI*2);
-    stage.addChild(shape3);
-    stage.update();
-}
-for(var a = 250; a > 0; a-=100)
-{
-    shape3.graphics.beginStroke("teal").arc((Math.sqrt((250*250)-(a*a))*-1)+shape2.x+100, (250-a)+135, 50, 0, Math.PI*2);
-    stage.addChild(shape3);
-    stage.update();
-}
+        var container = new createjs.Container();
 
-for(var a = 250; a > 0; a-=100)
-{
-    shape3.graphics.beginStroke("teal").arc((Math.sqrt((250*250)-(a*a))*-1)+shape2.x+100, a+100+shape2.y, 50, 0, Math.PI*2);
-    stage.addChild(shape3);
-    stage.update();
+        var bitmap = new createjs.Bitmap("images/slider/5.jpg");
+        bitmap.regX = bitmap.regY = 0;
+        bitmap.y = bitmap.x = 0;
+        bitmap.scaleX = bitmap.scaleY = 0.03;
+        var maskShape = new createjs.Shape();
+        maskShape.graphics.drawCircle(50,50,50);
+        bitmap.mask = maskShape;
+
+        container.addChild(bitmap);
+
+        stage.addChild(container);
+
+        container.x =x,
+            container.y = y;
 }
-setTimeout(function(){
+$("#canvasCVN").ready(function () {
     var bitmap = new createjs.Bitmap("images/slider/5.jpg");
     stage.addChild(bitmap);
-    stage.update();
     for(var a = 250; a > 0; a-=100)
     {
-        shape3.graphics.beginStroke("teal").arc(Math.sqrt((250*250)-(a*a))+shape2.x+100, (250-a)+135, 50, 0, Math.PI*2);
-        stage.addChild(shape3);
-        stage.update();
+        createImg(Math.sqrt((250*250)-(a*a))+shape2.x+50, (250-a)+85);
     }
     for(var a = 250; a > 0; a-=100)
     {
-        shape3.graphics.beginStroke("teal").arc(Math.sqrt((250*250)-(a*a))+shape2.x+100, a+100+shape2.y, 50, 0, Math.PI*2);
-        stage.addChild(shape3);
-        stage.update();
+        createImg(Math.sqrt((250*250)-(a*a))+shape2.x+50, a+50+shape2.y);
     }
     for(var a = 250; a > 0; a-=100)
     {
-        shape3.graphics.beginStroke("teal").arc((Math.sqrt((250*250)-(a*a))*-1)+shape2.x+100, (250-a)+135, 50, 0, Math.PI*2);
-        stage.addChild(shape3);
-        stage.update();
+        createImg((Math.sqrt((250*250)-(a*a))*-1)+shape2.x+50, (250-a)+85);
     }
 
     for(var a = 250; a > 0; a-=100)
     {
-        shape3.graphics.beginStroke("teal").arc((Math.sqrt((250*250)-(a*a))*-1)+shape2.x+100, a+100+shape2.y, 50, 0, Math.PI*2);
-        stage.addChild(shape3);
-        stage.update();
+        createImg((Math.sqrt((250*250)-(a*a))*-1)+shape2.x+50, a+50+shape2.y);
     }
-},4500);
+});
+setInterval(function(){stage.update()},10);
